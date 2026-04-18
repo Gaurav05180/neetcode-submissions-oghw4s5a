14faa153add9f93c58cb96b25b2238e1a7e77b6d
Gaurav Maurya <150830118+Gaurav05180@users.noreply.github.com>
2026-04-18 16:24:22 +0530

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+
+    vector<string> str;
+
+    string encode(vector<string>& strs) {
+        str = strs;
+
+        return "";
+    }
+
+    vector<string> decode(string s) {
+        return str;
+    }
+};
